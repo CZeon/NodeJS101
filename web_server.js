116c9d1fae43bca9c66754d7066fa84e29c96a5a
CZeon <maikonalexander.mabd@gmail.com>
2017-08-11 19:58:01 -0300

    Update web_server.js

--- web_server.js
+++ web_server.js
@@ -2,7 +2,7 @@ const http = require('http');
 const port = 8080;
 const server = http.createServer((req, res) => {
    res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end('kk eae men\n');
+   res.end('Hi Everybody!\n');
 });
 server.listen(port, () => {
   console.log(`Server running at http://your-ip-add-ress:${port}/`);
